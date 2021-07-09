9035b81acd969a4a8b4bc2467a56b071a5495eca
Zachary Beyer <zack@boomlearning.com>
2021-07-08 21:11:16 -0700

    Summon Magnum Opus instead of new token

--- ztest.js
+++ ztest.js
@@ -32,6 +32,15 @@ var spawnFxOnToken = function (tok, type)
 	spawnFx(tok.get("left"), tok.get("top"), type, tok.get("pageid"));
 };
 
+var spawnTokenAtXY = function (tokenJSON, pageID, spawnX, spawnY)
+{
+	let baseObj = JSON.parse(tokenJSON);
+	baseObj.pageid = pageID;
+	baseObj.left = spawnX;
+	baseObj.top = spawnY;
+	return createObj('graphic', baseObj);
+};
+
 var spawnTokenForToken = function (params)
 {
 	if (!params) return;
@@ -60,6 +69,60 @@ var spawnTokenForToken = function (params)
 		//aura1_color: "#ffff00",
 		showplayers_aura1: true
 	});
+};
+
+/**
+ * 	// https://app.roll20.net/forum/post/8991150/callback-function-question-for-defaulttoken
+ * @param name
+ * @returns {*}
+ */
+var spawnCharacterTokenNamed = function (inputName, msg)
+{
+	//let chars = findObjs({_type: "character", name: 'Magnum opus'}, {caseInsensitive: true});
+	//console.log(`chars: ${JSON.stringify(chars)}`);
+	//
+	//let defaultTokenString = 'waiting...';
+	//chars[0].get("_defaulttoken", (defaultToken) =>
+	//{
+	//	log(`CALLBACK: ${JSON.stringify(defaultToken)}`);
+	//
+	//	defaultTokenString = JSON.parse(defaultToken);
+	//	log(`IN THE CALLBACK: defaultTokenString = ${defaultTokenString}`);
+	//});
+
+	var selected = msg.selected;
+	if (selected === undefined)
+	{
+		sendChat("API", "Please select a character.");
+		return;
+	}
+
+	//set spawn point from selected  token properties
+	let tok = getObj("graphic", selected[0]._id);
+	let spawnPageID = tok.get("pageid");
+	let spawnLeft = tok.get("left") + 70;    //spawn to adjacent right of selected token (currently hardcoded, expand later with args?)
+	let spawnTop = tok.get("top");
+
+	let args = msg.content.split(/\s+--/);
+	args.shift();
+
+	if (inputName || (args.length >= 1))
+	{
+		inputName = inputName || args[0];
+		var check = findObjs({_type: "character", name: inputName}, {caseInsensitive: true})[0];
+
+		if (typeof check == 'undefined')
+		{
+			sendChat(msg.who, "Character named \"" + inputName + "\" not found.");
+		} else
+		{
+			var chars = findObjs({_type: "character", name: inputName}, {caseInsensitive: true});
+			chars[0].get("_defaulttoken", function (defaultToken)
+			{
+				spawnTokenAtXY(defaultToken, spawnPageID, spawnLeft, spawnTop);
+			});
+		}
+	}
 };
 
 var prettyPrintObj = function (object)
@@ -141,17 +204,17 @@ var spawnHolyWeapon = function (token)
 	sendChat(token.get("name"), "I summoned a spiritual weapon.");
 };
 
-var spawnSpellBook = function (token)
+var spawnSpellBook = function (msg)
 {
+	var token = getFirstSelectedToken(msg.selected);
 	if (!token) return;
-	log('token: ' + prettyPrintObj(token));
-
-	var newToken = spawnTokenForToken({
-		token: token, name: 'Book', imgSrc: ZBEYER_IMAGE_LIB.jakeBook
-	});
-
-	spawnFxOnToken(newToken, "burn-charm");
-	spawnFxOnToken(newToken, "burst-charm");
+	// OLD
+	// var newToken = spawnTokenForToken({
+	// 	token: token, name: 'Book', imgSrc: ZBEYER_IMAGE_LIB.jakeBook
+	// });
+	spawnCharacterTokenNamed('Magnum opus', msg);
+	spawnFxOnToken(token, "burn-charm");
+	spawnFxOnToken(token, "burst-charm");
 	var skillName = "MANIFEST MIND";
 	sendChat(token.get("name"), "activated " + skillName);
 };
@@ -183,44 +246,12 @@ var zbeyerMain = function (msg)
 	switch (cleanCommand)
 	{
 		case COMMAND_LIB.SUMMON_BOOK.toLowerCase():
-			var token = getFirstSelectedToken(msg.selected);
-			spawnSpellBook(primaryToken);
+			spawnSpellBook(msg);
 			break;
 		case COMMAND_LIB.SUMMON_SPIRIT_WEAPON.toLowerCase():
 			spawnHolyWeapon(primaryToken);
 			break;
 		case COMMAND_LIB.TEST.toLowerCase():
-			var character = findObjs({
-					type: 'character',
-					name: 'Magnum opus'
-				},
-				//options
-			)[0];
-
-			var characterToken = findObjs({
-					id: character._defaulttoken,
-					//type: 'graphic',
-				},
-				//options
-			)[0];
-			log(prettyPrintObj(character));
-			log(prettyPrintObj(characterToken));
-
-			if (!primaryToken) return;
-			//var character = getObj("character", token.get("represents"));
-			//var playerlist = (character && character.get("controlledby")) || token.get("controlledby");
-
-			//var newToken =
-			//
-			//	spawnTokenForToken({
-			//	id: _defaulttoken
-			//});
-
-			//spawnFxOnToken(newToken, "burn-charm");
-			//spawnFxOnToken(newToken, "burst-charm");
-			//var skillName = "MANIFEST MIND";
-			//sendChat(token.get("name"), "activated " + skillName);
-
 			break;
 		default:
 			//printText('command not found: ' + cleanCommand);
@@ -233,6 +264,8 @@ var zbeyerMain = function (msg)
  */
 on("ready", function ()
 {
+	//const tokenMarkers = JSON.parse(Campaign().get("token_markers"));
+	//log(tokenMarkers);
 	on("chat:message", function (msg)
 	{
 		var commandIndex = msg.content.indexOf("!");
