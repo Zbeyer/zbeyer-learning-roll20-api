a0c9bb670dd856d7365e3f5acacc60c7c7e00170
Zachary Beyer <zack@boomlearning.com>
2021-07-08 20:00:18 -0700

    initial commit

---
+++ ztest.js
@@ -0,0 +1,317 @@
+/*
+	msg
+		"{\"content\":\"<div style='width: 100%; border-radius: 4px;  box-shadow: 1px 1px 1px #707070; text-align: center; vertical-align: middle; padding: 3px 0px; margin: 0px auto; border: 1px solid #000; color: #000; background-image: -webkit-linear-gradient(-45deg, #a7c7dc 0%,#85b2d3 100%);'><b>API READY</b></div\",\"playerid\":\"API\",\"target\":\"gm\",\"target_name\":\"GM\",\"type\":\"whisper\",\"who\":\"HealthColors\"}"
+		"{\"content\":\"<div style='border: 3px solid #808080; background-color: #4B0082; color: white; padding: 1px 1px;'><div style=\\\"text-align: left;  margin: 5px 5px;\\\"><a style=\\\"position:relative;z-index:1000;float:left; background-color:transparent;border:0;padding:0;margin:0;display:block;\\\" href=\\\"!tm ping-target -Mc2tJUUVZH0c674A2j1\\\"><img src='https://s3.amazonaws.com/files.d20.io/images/152819033/7KQNEBUvnRkDkwml2UDJEQ/thumb.png?1595639986555555' style='width:70px; height:70px; padding: 0px 2px;' /></a><span style='font-family: Baskerville, \\\"Baskerville Old Face\\\", \\\"Goudy Old Style\\\", Garamond, \\\"Times New Roman\\\", serif;text-decoration: underline;font-size: 130%;'>Urmrratx'h</span>'s turn is done.</div><div style=\\\"text-align: right; margin: 5px 5px; position: relative; vertical-align: text-bottom;\\\"><a style=\\\"position:relative;z-index:1000;float:right; background-color:transparent;border:0;padding:0;margin:0;display:block;\\\" href=\\\"!tm ping-target -Mc2tJUtSn7GhJ8XgZlo\\\"><img src='https://s3.amazonaws.com/files.d20.io/images/218370078/iyrF-ActWWMerIcNiW8ABQ/thumb.png?161940672455555' style='width:70px; height:70px; padding: 0px 2px;' /></a><span style=\\\"position:absolute; bottom: 0;right:76px;\\\"><span style='font-family: Baskerville, \\\"Baskerville Old Face\\\", \\\"Goudy Old Style\\\", Garamond, \\\"Times New Roman\\\", serif;text-decoration: underline;font-size: 130%;'>Professor</span>, it's now your turn!</span><div style=\\\"clear:both;\\\"></div></div><a style=\\\"position:relative;z-index:10000; top:-1em;float: right;font-size: .6em; color: white; border: 1px solid #cccccc; border-radius: 1em; margin: 0 .1em; font-weight: bold; padding: .1em .4em;\\\" href=\\\"!eot\\\">EOT &#x21e8;</a><div style=\\\"padding: 5px;text-align: center;font-size: 100%;background-color: #ff00ff;text-shadow: -1px -1px 1px #000, 1px -1px 1px #000,-1px  1px 1px #000, 1px  1px 1px #000;letter-spacing: 3px;line-height: 130%;\\\">PROFESSOR</div><div style=\\\"clear:both;\\\"></div></div>\",\"playerid\":\"API\",\"type\":\"direct\",\"who\":\"\"}"
+		"{\"content\":\"test\",\"playerid\":\"-M9ep3SVVPHGFbunQN4I\",\"type\":\"general\",\"who\":\"Zachary B. (GM)\"}"
+		"{\"content\":\"bacon\",\"playerid\":\"-M9ep3SVVPHGFbunQN4I\",\"type\":\"general\",\"who\":\"Zachary B. (GM)\"}"
+		"{\"content\":\"$[[0]]\",\"inlinerolls\":[{\"expression\":\"1d4\",\"results\":{\"resultType\":\"sum\",\"rolls\":[{\"dice\":1,\"results\":[{\"v\":2}],\"rollid\":\"-Me77B2CsnD81D9LzjD4\",\"sides\":4,\"type\":\"R\"}],\"total\":2,\"type\":\"V\"},\"rollid\":\"-Me77B2CsnD81D9LzjD4\",\"signature\":\"7f420dbd4751c0d8f06f5215c260eb68072d5443b5b2071ebfb853c23536a32eaf75d951f0c01fa411e18b7f37fe0e64e015315bf4e8052ad6e7e4a94fb6c50b\",\"tdseed\":3389880423984014000}],\"playerid\":\"-M9ep3SVVPHGFbunQN4I\",\"type\":\"general\",\"who\":\"Zachary B. (GM)\"}"
+		"SPEAKINGAS: Example, MESSAGE: foo, CALLBACK: undefined, OPTIONS: undefined"
+		"zbeyer Main {\"content\":\"!foo\",\"playerid\":\"-M9ep3SVVPHGFbunQN4I\",\"type\":\"api\",\"who\":\"Zachary B. (GM)\"}"
+		"{\"content\":\"foo\",\"playerid\":\"API\",\"type\":\"general\",\"who\":\"Example\"}"
+ */
+
+/**
+ {
+ 	Token example
+	"_id": "-Mc2CVgggYbO1xSDnZX6",
+	"_pageid": "-M_NAqT3zMSlKcZHMnij",
+	"left": 735,
+	"top": 1295,
+	"width": 70,
+	"height": 70,
+	"rotation": 0,
+	"layer": "objects",
+	"isdrawing": false,
+	"flipv": false,
+	"fliph": false,
+	"imgsrc": "https://s3.amazonaws.com/files.d20.io/images/222853205/Ig0Okq-ZXy1lWYiAjPLmjA/thumb.png?16212210335555",
+	"name": "MANIFEST MIND",
+	"gmnotes": "",
+	"controlledby": "-MGzjeQFz2btCnlCxjE_",
+	"bar1_value": "1",
+	"bar1_max": "1",
+	"bar1_link": "",
+	"bar2_value": "1",
+	"bar2_max": "1",
+	"bar2_link": "",
+	"bar3_value": "4",
+	"bar3_max": "4",
+	"bar3_link": "",
+	"represents": "",
+	"aura1_radius": 0.675,
+	"aura1_color": "#05ff00",
+	"aura1_square": false,
+	"aura2_radius": 0.037500000000000006,
+	"aura2_color": "#ffffff",
+	"aura2_square": false,
+	"tint_color": "transparent",
+	"statusmarkers": "",
+	"showname": true,
+	"showplayers_name": true,
+	"showplayers_bar1": false,
+	"showplayers_bar2": false,
+	"showplayers_bar3": false,
+	"showplayers_aura1": true,
+	"showplayers_aura2": true,
+	"playersedit_name": true,
+	"playersedit_bar1": true,
+	"playersedit_bar2": true,
+	"playersedit_bar3": true,
+	"playersedit_aura1": true,
+	"playersedit_aura2": true,
+	"light_radius": "",
+	"light_dimradius": "",
+	"light_otherplayers": false,
+	"light_hassight": false,
+	"light_angle": "",
+	"light_losangle": "",
+	"light_multiplier": 1,
+	"adv_fow_view_distance": "",
+	"sides": "",
+	"currentSide": 0,
+	"lastmove": "665,1295",
+	"_type": "graphic",
+	"_subtype": "token",
+	"_cardid": "",
+	"has_bright_light_vision": false,
+	"has_night_vision": false,
+	"night_vision_tint": null,
+	"night_vision_distance": 0,
+	"emits_bright_light": false,
+	"bright_light_distance": 0,
+	"emits_low_light": false,
+	"low_light_distance": 0,
+	"has_limit_field_of_vision": false,
+	"limit_field_of_vision_center": 0,
+	"limit_field_of_vision_total": 0,
+	"has_limit_field_of_night_vision": false,
+	"limit_field_of_night_vision_center": 0,
+	"limit_field_of_night_vision_total": 0,
+	"has_directional_bright_light": false,
+	"directional_bright_light_total": 0,
+	"directional_bright_light_center": 0,
+	"has_directional_low_light": false,
+	"directional_low_light_total": 0,
+	"directional_low_light_center": 0,
+	"light_sensitivity_multiplier": 100,
+	"night_vision_effect": null,
+	"dim_light_opacity": "0.75",
+	"bar_location": null,
+	"compact_bar": null
+
+	[
+	{
+		"name": "Magnum opus",
+		"bio": "",
+		"gmnotes": "",
+		"_defaulttoken": 1625792429462,
+		"archived": false,
+		"inplayerjournals": "-MGzjeQFz2btCnlCxjE_",
+		"controlledby": "-MGzjeQFz2btCnlCxjE_",
+		"_id": "-Me7fXITOBAESyC-Vn80",
+		"_type": "character",
+		"avatar": "https://s3.amazonaws.com/files.d20.io/images/233035273/hCqQMJSyCAurm9ka_xP7Mw/med.jpg?1625790824"
+	}
+]
+}
+ */
+
+/**
+ * Utilities
+ **/
+
+var ZBEYER_IMAGE_LIB = {
+	jakeBook: 'https://s3.amazonaws.com/files.d20.io/images/222853205/Ig0Okq-ZXy1lWYiAjPLmjA/thumb.png?16212210255'
+};
+
+var COMMAND_LIB = {
+	SUMMON_BOOK: ('knowledgeIsPower').toLowerCase(),
+	SUMMON_SPIRIT_WEAPON: ('spiritIsWilling').toLowerCase(),
+	TEST: ('foo').toLowerCase(),
+};
+
+
+var getFirstSelectedToken = function (selected)
+{
+	if (!selected) return;
+	return getObj("graphic", selected[0]._id);
+};
+
+var spawnFxOnToken = function (tok, type)
+{
+	type = type || "burst-holy";
+	spawnFx(tok.get("left"), tok.get("top"), type, tok.get("pageid"));
+};
+
+var spawnTokenForToken = function (params)
+{
+	if (!params) return;
+	var token = params.token;
+	var imgSrc = params.imgSrc;
+	var name = params.name;
+
+	if (!token) return;
+	var character = getObj("character", token.get("represents"));
+	var playerlist = (character && character.get("controlledby")) || token.get("controlledby");
+
+	name = token.get("name") + "'s " + (name || "Spiritual Weapon");
+	imgSrc = imgSrc || "https://s3.amazonaws.com/files.d20.io/images/104629087/ZzHGu7CiyzL0sZTuB8KfJA/thumb.png?15816820805";
+
+	return createObj("graphic", {
+		left: token.get("left") + 45,
+		top: token.get("top"),
+		height: 70,
+		width: 70,
+		pageid: token.get("pageid"),
+		layer: "objects",
+		imgsrc: imgSrc,
+		name: name,
+		controlledby: playerlist ? playerlist : null,
+		//aura1_radius: 0,
+		//aura1_color: "#ffff00",
+		showplayers_aura1: true
+	});
+};
+
+var prettyPrintObj = function (object)
+{
+	// return JSON.stringify(object, null, "\t"); // stringify with tabs inserted at each level
+	return JSON.stringify(object);
+};
+
+var stringByRemovingSubstring = function (string, subString)
+{
+	//const substrings = 'Hello World'.split('Wo'); // ['Hello ', 'rld']
+	const substrings = string.split(subString);
+	return substrings.join(''); // 'Hello rld';
+};
+
+var getCommand = function (msg)
+{
+	var msgString = (msg && msg.content);
+	if (!msgString) return;
+	msgString = msgString.toLowerCase().trim();
+	var endCommandIndex = msgString.indexOf(' ');
+	if (endCommandIndex === -1) return msgString;
+
+	var apiCommandFlag = '!';
+	var commandIndex = msgString.indexOf(apiCommandFlag);
+
+	return msgString.slice(apiCommandFlag, endCommandIndex);
+};
+
+var printText = function (SPEAKINGAS, MESSAGE, CALLBACK, OPTIONS)
+{
+	log('SPEAKINGAS: ' + SPEAKINGAS + ', MESSAGE: ' + MESSAGE + ', CALLBACK: ' + CALLBACK + ', OPTIONS: ' + OPTIONS);
+	sendChat(SPEAKINGAS, MESSAGE, CALLBACK, OPTIONS);
+};
+
+
+/**
+ * Scripts
+ */
+
+
+var spawnHolyWeapon = function (token)
+{
+	if (!token) return;
+	log('token: ' + prettyPrintObj(token));
+
+	var newToken = spawnTokenForToken({token: token});
+	spawnFxOnToken(token);
+	sendChat(token.get("name"), "I summoned a spiritual weapon.");
+};
+
+var spawnSpellBook = function (token)
+{
+	if (!token) return;
+	log('token: ' + prettyPrintObj(token));
+
+	var newToken = spawnTokenForToken({
+		token: token, name: 'Book', imgSrc: ZBEYER_IMAGE_LIB.jakeBook
+	});
+
+	spawnFxOnToken(newToken, "burn-charm");
+	spawnFxOnToken(newToken, "burst-charm");
+	var skillName = "MANIFEST MIND";
+	sendChat(token.get("name"), "activated " + skillName);
+};
+
+/**
+ * Main
+ */
+var zbeyerMain = function (msg)
+{
+	var command = getCommand(msg);
+	//log('command '+ command);
+
+	var cleanCommand = stringByRemovingSubstring(command, '!');
+	var cleanMessage = stringByRemovingSubstring(msg.content, command);
+	if (!command) return;
+	if (!cleanCommand) return;
+	//log('cleanCommand: ' + cleanCommand);
+	//log('cleanMessage: ' + cleanMessage);
+
+	var primaryToken = getFirstSelectedToken(msg.selected);
+	log('primaryToken: ' + prettyPrintObj(primaryToken));
+	//var name = (primaryToken && primaryToken.get("name")) || 'Example';
+
+	switch (cleanCommand)
+	{
+		case COMMAND_LIB.SUMMON_BOOK.toLowerCase():
+			var token = getFirstSelectedToken(msg.selected);
+			spawnSpellBook(primaryToken);
+			break;
+		case COMMAND_LIB.SUMMON_SPIRIT_WEAPON.toLowerCase():
+			spawnHolyWeapon(primaryToken);
+			break;
+		case COMMAND_LIB.TEST.toLowerCase():
+			var character = findObjs({
+					type: 'character',
+					name: 'Magnum opus'
+				},
+				//options
+			)[0];
+
+			var characterToken = findObjs({
+					id: character._defaulttoken,
+					//type: 'graphic',
+				},
+				//options
+			)[0];
+			log(prettyPrintObj(character));
+			log(prettyPrintObj(characterToken));
+
+			if (!primaryToken) return;
+			//var character = getObj("character", token.get("represents"));
+			//var playerlist = (character && character.get("controlledby")) || token.get("controlledby");
+
+			//var newToken =
+			//
+			//	spawnTokenForToken({
+			//	id: _defaulttoken
+			//});
+
+			//spawnFxOnToken(newToken, "burn-charm");
+			//spawnFxOnToken(newToken, "burst-charm");
+			//var skillName = "MANIFEST MIND";
+			//sendChat(token.get("name"), "activated " + skillName);
+
+			break;
+		default:
+			//printText('command not found: ' + cleanCommand);
+			break;
+	}
+};
+
+on("ready", function ()
+{
+	on("chat:message", function (msg)
+	{
+		var commandIndex = msg.content.indexOf("!");
+		if (msg.type == "api" && (commandIndex !== -1))
+		{
+			zbeyerMain(msg);
+		}
+	});
+});
